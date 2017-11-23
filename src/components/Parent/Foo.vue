78ad914b5547f362a47b65e6c9a3ef5c84f90355
Sastha <shazcool@gmail.com>
2017-11-23 19:03:49 +0530

    add lazy loading of components

--- src/components/Parent/Foo.vue
+++ src/components/Parent/Foo.vue
@@ -1,14 +1,18 @@
-<template>
-  <el-row :gutter="10">
-  	<el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24"><div class="foo">foo</div></el-col>
-  </el-row>
-</template>
+const Foo = () => Promise.resolve({ 
+	<template>
+	  <el-row :gutter="10">
+	  	<el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24"><div class="foo">foo</div></el-col>
+	  </el-row>
+	</template>
 
-<script>
+	<script>
 
-</script>
+	</script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+	<!-- Add "scoped" attribute to limit CSS to this component only -->
+	<style scoped>
 
-</style>
+	</style>
+
+})
+
