c4057d993d252d9b82542c4721940cbe706e344d
Sastha <shazcool@gmail.com>
2017-12-01 21:30:33 +0530

    add notifications

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <el-button type="primary" @click="openAutoCloseNotification">Auto Close Notification</el-button>
+    <el-button type="primary" @click="openNotification">Notification</el-button>
+    <el-button type="primary" @click="openSuccess">Success</el-button>
+    <el-button type="primary" @click="openWarning">Warning</el-button>
+    <el-button type="primary" @click="openInfo">Info</el-button>
+    <el-button type="primary" @click="openError">Error</el-button>
   </div>
 </template>
 
@@ -10,6 +16,57 @@ export default {
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+    openNotification () {
+      this.$notify({
+        title: 'Prompt',
+        message: 'This is a message that does not automatically close',
+        duration: 0
+      })
+    },
+    openAutoCloseNotification () {
+      const h = this.$createElement
+
+      this.$notify({
+        title: 'Title',
+        message: h('i', { style: 'color: teal' }, 'This is a reminder')
+      })
+    },
+    openSuccess () {
+      const h = this.$createElement
+
+      this.$notify({
+        title: 'Title',
+        message: h('i', { style: 'color: teal' }, 'This is a reminder'),
+        type: 'success'
+      })
+    },
+    openWarning () {
+      const h = this.$createElement
+
+      this.$notify({
+        title: 'Title',
+        message: h('i', { style: 'color: teal' }, 'This is a reminder'),
+        type: 'warning'
+      })
+    },
+    openInfo () {
+      const h = this.$createElement
+
+      this.$notify.info({
+        title: 'Title',
+        message: h('i', { style: 'color: teal' }, 'This is a reminder')
+      })
+    },
+    openError () {
+      const h = this.$createElement
+
+      this.$notify.error({
+        title: 'Title',
+        message: h('i', { style: 'color: teal' }, 'This is a reminder')
+      })
     }
   }
 }
