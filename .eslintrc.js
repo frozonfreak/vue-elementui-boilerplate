0a5b3bdecad56433fcdee58e00eb6e7cf30ba113
Sastha <shazcool@gmail.com>
2017-12-28 13:50:33 +0530

    Revert "relax eslint rules."
    This reverts commit 4b8325a831b87d601f34b10074f5647d86c5a4fb.

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,8 +10,7 @@ module.exports = {
     browser: true,
   },
   // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-  //extends: 'standard',
-  extends: 'eslint:recommended',
+  extends: 'standard',
   // required to lint *.vue files
   plugins: [
     'html'
